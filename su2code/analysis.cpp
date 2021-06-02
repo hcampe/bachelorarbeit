0eda35fa54e21ac37ef68896bcc5a324925e244f
Heinrich Campe <s6hevonc@uni-bonn.de>
2021-06-02 10:58:16 +0200

    tried another version of multi threading for gaugeEnergy: 5 sweeps still take 35.6869s.

--- su2code/analysis.cpp
+++ su2code/analysis.cpp
@@ -14,21 +14,59 @@ SU2matrix getPlaquette(const Gaugeconfig& U,
 	return U(x, mu)*U(xPlusMu, nu)*U(xPlusNu, mu).dagger()*U(x, nu).dagger();
 }
 
-
 double gaugeEnergy(const Gaugeconfig& U)
 {
 	double energy { 0. };
-	long int timeSize { static_cast<long int>(U.getTimeSize()) };
-	long int spaceSize { static_cast<long int>(U.getSpaceSize()) };
+
+    const int numberOfThreads { omp_get_max_threads() };
+    static double* energyOfThread = new double[numberOfThreads];
+
+    #pragma omp parallel
+    {
+        int thread { omp_get_thread_num() };
+        
+        #pragma omp for
+	    for (long int x0 = 0; x0 < U.getTimeSize(); x0++)
+	    {
+	        for (long int x1 = 0; x1 < U.getSpaceSize(); x1++)
+	    	{
+	            for (long int x2 = 0; x2 < U.getSpaceSize(); x2++)
+	    		{
+	                for (long int x3 = 0; x3 < U.getSpaceSize(); x3++)
+	    			{
+	    				for (std::size_t nu { 1 }; nu < Gaugeconfig::numSpacetimeDim; nu++)
+	    				{
+	    					for (std::size_t mu { 0 }; mu < nu; mu++)
+	    					{
+	    						energyOfThread[thread] = real(trace(getPlaquette(U, {x0, x1, x2, x3}, mu, nu)));
+	    					}
+	    				}
+	    			}
+	    		}
+	    	}
+	    }
+    }
+
+    for (int i {0}; i < numberOfThreads; i++)
+    {
+        energy += energyOfThread[i];
+    }
+
+	return energy;
+}
+
+double gaugeEnergy2(const Gaugeconfig& U)
+{
+	double energy { 0. };
 
     #pragma omp parallel for reduction(+:energy)
-	for (long int x0 = 0; x0 < timeSize; x0++)
+	for (long int x0 = 0; x0 < U.getTimeSize(); x0++)
 	{
-	    for (long int x1 = 0; x1 < spaceSize; x1++)
+	    for (long int x1 = 0; x1 < U.getSpaceSize(); x1++)
 		{
-	        for (long int x2 = 0; x2 < spaceSize; x2++)
+	        for (long int x2 = 0; x2 < U.getSpaceSize(); x2++)
 			{
-	            for (long int x3 = 0; x3 < spaceSize; x3++)
+	            for (long int x3 = 0; x3 < U.getSpaceSize(); x3++)
 				{
 					for (std::size_t nu { 1 }; nu < Gaugeconfig::numSpacetimeDim; nu++)
 					{
