d31b5a288c12af007f0f53908cf7f3bbc3954727
Heinrich Campe <s6hevonc@uni-bonn.de>
2021-06-02 11:38:02 +0200

    more precise measurement: 5 sweeps take 35.r115s

--- su2code/main.cpp
+++ su2code/main.cpp
@@ -4,6 +4,7 @@
 #include <fstream> // to log things to file
 #include <iostream>
 #include <random> // for the random generator
+#include <sys/time.h> // to measure the computation time
 
 #include "analysis.h"
 #include "Gaugeconfig.h"
@@ -39,6 +40,10 @@ int main()
 	const std::string filename { "frmColdStart.txt" };
 	std::vector<double> energy(numberOfSweeps, 0.);
 
+    // to measure the time:
+    struct timeval tStart, tEnd;
+    gettimeofday(&tStart, NULL);
+
 	std::cout << "performing " << numberOfSweeps << " sweeps: ";
 	for (std::size_t i {0}; i < numberOfSweeps; i++)
 	{
@@ -48,6 +53,13 @@ int main()
 	}
 	std::cout << '\n';
 
+    gettimeofday(&tEnd, NULL);
+
+    const double sec {static_cast<double>(tEnd.tv_sec - tStart.tv_sec) };
+    const double usec { static_cast<double>(tEnd.tv_usec - tStart.tv_usec) };
+
+    std::cout << numberOfSweeps << " sweeps take " << sec + 1.e-6*usec << "s" << std::endl;
+ 
 	// std::cout << "zero if writing successful: ";
 	// std::cout << writeVector(energy, dataDir + filename) << '\n';
 
